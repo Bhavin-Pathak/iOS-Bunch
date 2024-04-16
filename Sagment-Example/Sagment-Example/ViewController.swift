948c64747e9e219993e73053be2dbf17969ac905
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-16 14:17:07 +0530

    chore: connect images to selected indexed

--- Sagment-Example/Sagment-Example/ViewController.swift
+++ Sagment-Example/Sagment-Example/ViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 class ViewController: UIViewController {
 
    
+    @IBOutlet weak var imageview: UIImageView!
     @IBOutlet weak var sagmentout: UISegmentedControl!
-    
     @IBOutlet weak var lableforans: UILabel!
     @IBAction func SelectedSagment(_ sender: Any) {
         
-        
+        if sagmentout.selectedSegmentIndex == 0{
+            lableforans.text = "CODE IS SELECTED" // CODE
+            imageview.image = UIImage(named: "code")
+            imageview.isHidden = false
+            lableforans.isHidden = false
+        }
+        else if sagmentout.selectedSegmentIndex == 1{
+            lableforans.text = "API IS SELECTED" // API
+            imageview.image = UIImage(named: "api")
+            imageview.isHidden = false
+            lableforans.isHidden = false
+        }
+        else if sagmentout.selectedSegmentIndex == 2{
+            lableforans.text = "DB IS SELECTED" // DB
+            imageview.image = UIImage(named: "db")
+            imageview.isHidden = false
+            lableforans.isHidden = false
+        }
+        else if sagmentout.selectedSegmentIndex == 3{
+            lableforans.text = "SAS IS SELECTED" // SAS
+            imageview.image = UIImage(named: "sas")
+            imageview.isHidden = false
+            lableforans.isHidden = false
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageview.isHidden = true
+        lableforans.isHidden = true
     }
     
 }
