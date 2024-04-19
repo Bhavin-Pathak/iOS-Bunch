362d4913abb0ac828475d22445fc7a65aa85b19e
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-19 12:10:41 +0530

    initial commit

---
+++ IBoutlate-actions-CollectionsofBOTH/IBoutlate-actions-CollectionsofBOTH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBoutlate-actions-CollectionsofBOTH
+//
+//  Created by MacBookPro on 30/01/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
