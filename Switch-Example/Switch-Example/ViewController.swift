e6190367ed285e1af475f5ef96240f9e93f22eef
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-19 12:08:29 +0530

    chore: on-off status added

--- Switch-Example/Switch-Example/ViewController.swift
+++ Switch-Example/Switch-Example/ViewController.swift
@@ -10,18 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    
+    //MARK: Outlate as var
     @IBOutlet weak var switchb: UISwitch!
     
     @IBOutlet weak var lablet: UILabel!
     
-
+//MARK: Function That on off switch Status
     @IBAction func status(_ sender: UISwitch) {
+        
+        if switchb.isOn {
+            lablet.text = "Switch is On"
+            lablet.isHidden = false
+            self.view.backgroundColor = UIColor.blue
+        }else{
+            lablet.text = "Switch is Off"
+            lablet.isHidden = false
+            self.view.backgroundColor = UIColor.black
+            lablet.textColor = UIColor.white
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lablet.isHidden = true
     }
 
 
