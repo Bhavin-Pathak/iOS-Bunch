eba960ec61bcf9241bf1f12399e1103f5f86fcbf
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-19 20:13:26 +0530

    chore: adding functions to make arreange buttons actions on tap

--- IBoutlate-actions-CollectionsofBOTH/IBoutlate-actions-CollectionsofBOTH/ViewController.swift
+++ IBoutlate-actions-CollectionsofBOTH/IBoutlate-actions-CollectionsofBOTH/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     
     //MARK: Collections of buttons that are connectd with storybord
+    // Frist create outlet -> Connect more panding buttons with control... on storybord
     @IBOutlet var buttons: [UIButton]!
     
     
@@ -18,8 +19,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    //MARK: Action or status button
+    //create action(UIButton) -> Connect more panding buttons with control... on storybord
     @IBAction func buttonactionofstatus(_ sender: UIButton) {
+        for bun in buttons{
+            bun.isSelected = false
+        }
+        sender.isSelected.toggle()
         
     }
     
