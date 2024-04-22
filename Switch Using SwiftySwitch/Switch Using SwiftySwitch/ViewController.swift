a4798c5b2e37612783cb668f0293514091c63490
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-22 22:07:55 +0530

    chore: adding to VC With Delicate Method

--- Switch Using SwiftySwitch/Switch Using SwiftySwitch/ViewController.swift
+++ Switch Using SwiftySwitch/Switch Using SwiftySwitch/ViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SwiftySwitchDelegate {
+    func valueChanged(sender: SwiftySwitch) {
+        if switchTo.isOn{
+            view.backgroundColor = UIColor(ciColor: .blue)
+        }else{
+            view.backgroundColor = UIColor(ciColor: .red)
+        }
+    }
+    
 
+    @IBOutlet weak var switchTo: SwiftySwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switchTo.delegate = self
     }
 
 
