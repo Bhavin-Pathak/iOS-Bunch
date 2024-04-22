828b6ddadae005adb69d06213be8070c95bccd87
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-22 23:57:56 +0530

    chore: validation methods added

--- Validation of Email Password PhoneNumber/Validation of Email Password PhoneNumber/ViewController.swift
+++ Validation of Email Password PhoneNumber/Validation of Email Password PhoneNumber/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailtF: UITextField!
+    
+    @IBOutlet weak var passwordtF: UITextField!
+    
+    @IBOutlet weak var phonetF: UITextField!
+    
+    
+    
+    @IBAction func submitAction(_ sender: UIButton) {
+        if (emailtF.text?.isValidEmail)! {
+            print("Valid Email")
+        }
+        else{
+            print("Invalid Email")
+            
+        }
+        if (passwordtF.text?.isValidPassword)!{
+            print("Valid Password")
+        }else{
+            print("Invalid Password")
+        }
+        if (phonetF.text?.isValidPhone)!{
+            print("Valid Phone Number")
+        }else{
+            print("Invalid Phone Number")
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
