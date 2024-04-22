12c8c9fededd3d3d99254f2aac1498ca488a0e0c
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-22 14:35:18 +0530

    initial commit

---
+++ UITextField Auto-Capitalization/UITextField Auto-Capitalization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextField Auto-Capitalization
+//
+//  Created by MacBookPro on 02/02/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
