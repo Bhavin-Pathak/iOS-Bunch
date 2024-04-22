95e04b10e0681717c8e5f108300a4319d9f85024
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-22 19:31:04 +0530

    chore: connect with VC And Capitalisation Example Added

--- UITextField Auto-Capitalization/UITextField Auto-Capitalization/ViewController.swift
+++ UITextField Auto-Capitalization/UITextField Auto-Capitalization/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf1: UITextField!
+    @IBOutlet weak var tf2: UITextField!
+    @IBOutlet weak var tf3: UITextField!
+    @IBOutlet weak var tf4: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: Some Examples Of AutoCapitalization
+        tf1.autocapitalizationType = .allCharacters
+        tf2.autocapitalizationType = .none
+        tf3.autocapitalizationType = .sentences
+        tf4.autocapitalizationType = .words
     }
-
-
+    //MARK: There Are 4 Properties Of TF
+    // 1 :- "All Charectoers" is the same as double tapping the shiftkey basically capsLock
+    // 2 :- "none" is pretty selft-explanatory keybord will nver try to capitlize letters
+    // 3 :- "Sentance" will try to Capitlize the next word after em mark punch
+    // 4 :- "Words" will try to capitilize every new word after a space
 }
 
