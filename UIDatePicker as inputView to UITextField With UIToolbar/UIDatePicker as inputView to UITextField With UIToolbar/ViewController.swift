ebfb34e84dbbe8156a053b71078ae5d6a91d0eda
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-22 13:54:33 +0530

    chore: methods Added For Open Date Picker in TF

--- UIDatePicker as inputView to UITextField With UIToolbar/UIDatePicker as inputView to UITextField With UIToolbar/ViewController.swift
+++ UIDatePicker as inputView to UITextField With UIToolbar/UIDatePicker as inputView to UITextField With UIToolbar/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfDate: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: Call Deligate When View Load
+        tfDate.delegate = self
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate{
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.openDatePiker()
+    }
+}
+
+//MARK: Make Extenction For Date Picker FOR VC
+extension ViewController{
+    //MARK: Function That Open DATEPICKER
+    func openDatePiker()  {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(dpHendler(datePicker: )), for: .valueChanged)
+        
+        
+        tfDate.inputView = datePicker //MARK: KEYBORD
+        //MARK: Keybord Toolbar Frame
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 40))
+        //MARK: Calcel Button Property
+        let cancleButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancleBtnClick))
+        //MARK: Ok Button Property
+        let okButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(okBtnClick))
+        //MARK: Flixible Space On Buttons According To Screen Size
+        let flexibleButtons = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        //MARK: Setitems On Toolbar
+        toolbar.setItems([cancleButton, flexibleButtons, okButton], animated: true)
+        tfDate.inputAccessoryView = toolbar
+    }
+    
+    @objc
+    func cancleBtnClick(){
+        //MARK: Hide Date Picker When User Click Cancel Button
+        tfDate.resignFirstResponder()
+        
+    }
+    
+    @objc
+    func okBtnClick(){
+        //MARK: Get Date From DatePicker In TFFIELD
+        if let datePicker = tfDate.inputView as? UIDatePicker{
+            //MARK: Date Formate
+            let formateDate = DateFormatter()
+            formateDate.dateStyle = .medium
+            tfDate.text = formateDate.string(from: datePicker.date)
+            
+            print(datePicker.date)
+        }
+        tfDate.resignFirstResponder()
+    }
+    
+    @objc
+    func dpHendler(datePicker: UIDatePicker){
+        print(datePicker.date)
+    }
+}
