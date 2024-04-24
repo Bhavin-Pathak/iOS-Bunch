aae3dd039eca1127aa75ff1b2cdf8762e360367b
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-24 17:19:47 +0530

    chore: Pushview PopView Added

--- NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/SecondScreen.swift
+++ NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/SecondScreen.swift
@@ -14,4 +14,15 @@ class SecondScreen: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func navigatetoThirdVC(_ sender: UIButton) {
+        let gotoThirdVC = self.storyboard?.instantiateViewController(withIdentifier: "ThirdScreen") as! ThirdScreen
+        //MARK: Asign Storybord Identifire Same as ViewController Name
+        self.navigationController?.pushViewController(gotoThirdVC, animated: true)
+    }
+    
+    @IBAction func navigateToMainScreen(_ sender: UIButton) {
+        //MARK:  GO BACK TO THAT SCRREN WHERE IT"S FROM NAVIGATE
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
