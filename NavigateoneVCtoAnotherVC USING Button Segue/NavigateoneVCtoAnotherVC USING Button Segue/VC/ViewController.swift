d69c4dcbcd37946ee8be919092c604f79e90d293
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-24 17:17:03 +0530

    chore: navigate to second screen added -pushview

--- NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/ViewController.swift
+++ NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: Navigation On Button Click To SecondScreen VC
+    @IBAction func navigateToSecondVC(_ sender: UIButton) {
+        //Methods to navigate to another VC
+        print("Buton Workss")
+        let gotoSecondVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondScreen") as! SecondScreen
+        //MARK: Asign Storybord Identifire Same as ViewController Name 
+        self.navigationController?.pushViewController(gotoSecondVC, animated: true)
+        
+    }
 }
 
