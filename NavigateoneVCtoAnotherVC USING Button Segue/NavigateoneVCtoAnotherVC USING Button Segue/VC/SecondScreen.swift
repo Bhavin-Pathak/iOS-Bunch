1b0effbc5e37a47bf36b2fa282e717a5d9b2ac17
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-24 15:33:30 +0530

    ViewController Added

---
+++ NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/SecondScreen.swift
@@ -0,0 +1,17 @@
+//
+//  SecondScreen.swift
+//  NavigateoneVCtoAnotherVC USING Button Segue
+//
+//  Created by MacBookPro on 04/02/1946 Saka.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ NavigateoneVCtoAnotherVC USING Button Segue/NavigateoneVCtoAnotherVC USING Button Segue/VC/ThirdScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdScreen.swift
+//  NavigateoneVCtoAnotherVC USING Button Segue
+//
+//  Created by MacBookPro on 04/02/1946 Saka.
+//
+
+import UIKit
+
+class ThirdScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
