3583eaa25f2e26592e39b998ed5f06a21af4ed55
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 12:33:34 +0530

    chore: notification extinction added

---
+++ PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Ext/extentions.swift
@@ -0,0 +1,16 @@
+//
+//  extentions.swift
+//  PassingData-using-NotificationCenter
+//
+//  Created by MacBookPro on 05/02/1946 Saka.
+//
+
+import Foundation
+import UIKit
+
+extension Notification.Name{
+
+    static let facebook = Notification.Name("facebook")
+    static let instagram = Notification.Name("instagram")
+    
+}
