5b078b8dea05cff4b897cb6cce96e596f60f698e
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 13:59:42 +0530

    chore: on selection delegate return imp nd text

--- PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Viewcontroller/SecondViewController.swift
+++ PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Viewcontroller/SecondViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBAction func clickFacebook(_ sender: Any) {
+        //MARK: Assign notificationcenter name to get data
+        NotificationCenter.default.post(name: .facebook, object: nil)
+        self.navigationController?.popToRootViewController(animated: true)
+        
+    }
+    
+    
+    @IBAction func clickInstagram(_ sender: Any) {
+        //MARK: Assign notificationcenter name to get data
+        NotificationCenter.default.post(name: .instagram, object: nil)
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
 }
