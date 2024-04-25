de0c64fac80d4f5ed82a7c31be0b3a7fb68a281f
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 10:56:43 +0530

    chore: create outlets and actions for navigate and send data to other screen

--- Passdata-oneVC-to-OtherVC/Passdata-oneVC-to-OtherVC/Views/ViewController.swift
+++ Passdata-oneVC-to-OtherVC/Passdata-oneVC-to-OtherVC/Views/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    //MARK: Outlets For Textfilds 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var emailAdddress: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //MARK: Save Button And Navigate To SecondVC
+    @IBAction func saveContinue(_ sender: UIButton) {
+        //Property of SecondVC
+        let secVC:SecondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        //Get Acess of varibles using property of secondVC and Assign Textfilds values to lable strings
+        secVC.name = userName.text ?? "Faild To Load Name"
+        secVC.email = emailAdddress.text ?? "Faild To Load Email Address"
+        self.navigationController?.pushViewController(secVC, animated: true)
+    }
+    
 
 
 }
