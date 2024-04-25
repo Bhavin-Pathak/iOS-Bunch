1eaec7b3ed199744ed5818427e3ff2a19e3bf86d
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 13:58:00 +0530

    chore: outlet connect selector functions added ad lbl ext nd img into this

--- PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Viewcontroller/ViewController.swift
+++ PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Viewcontroller/ViewController.swift
@@ -9,13 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: Outlets
+    @IBOutlet weak var selectionLbl: UILabel!
+    @IBOutlet weak var imgPreview: UIImageView!
     
-
+    @IBAction func chooseSocials(_ sender: Any) {
+        //MARK: Choose From SecondVC
+        let secVC = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(secVC, animated: true)
+    }
+    //Selector for facebook
+    @objc func selectFB(notification:Notification){
+        selectionLbl.text = "Selected Image Is Facebook"
+        imgPreview.image = UIImage(imageLiteralResourceName: "fb")
+    }
+    //Selector for instagram
+    @objc func selectIG(notification:Notification){
+        selectionLbl.text = "Selected Image Is Instagram"
+        imgPreview.image = UIImage(imageLiteralResourceName: "ig")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //MARK: When view loading frist
+        NotificationCenter.default.addObserver(self, selector: #selector(selectFB(notification: )), name: .facebook, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(selectIG(notification: )), name: .instagram, object: nil)
     }
 
 
 }
+
 
