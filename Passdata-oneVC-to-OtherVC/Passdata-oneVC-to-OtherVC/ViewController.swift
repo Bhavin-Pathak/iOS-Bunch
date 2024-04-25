824e32957a030373477a0449428cc4e95c7e60c5
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 10:07:00 +0530

    initial commit

---
+++ Passdata-oneVC-to-OtherVC/Passdata-oneVC-to-OtherVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passdata-oneVC-to-OtherVC
+//
+//  Created by MacBookPro on 05/02/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
