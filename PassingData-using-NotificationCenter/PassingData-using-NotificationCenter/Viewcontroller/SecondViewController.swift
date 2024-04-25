cf5821a130528f96992f98b511f2719d933aba24
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-25 11:50:45 +0530

    VC-added

---
+++ PassingData-using-NotificationCenter/PassingData-using-NotificationCenter/Viewcontroller/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  PassingData-using-NotificationCenter
+//
+//  Created by MacBookPro on 05/02/1946 Saka.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
