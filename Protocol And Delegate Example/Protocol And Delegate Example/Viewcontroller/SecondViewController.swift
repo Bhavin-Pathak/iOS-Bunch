dc9a8a711c22c0e4b6a96e68c7ec94d26d711e8e
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-26 13:16:35 +0530

    outlets create

---
+++ Protocol And Delegate Example/Protocol And Delegate Example/Viewcontroller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Protocol And Delegate Example
+//
+//  Created by MacBookPro on 05/02/1946 Saka.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    //MARK: Outlets
+    @IBOutlet weak var tatName: UITextField!
+    @IBOutlet weak var tatEmail: UITextField!
+    @IBOutlet weak var tatAddress: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: SAve Data on Click
+    @IBAction func saveData(_ sender: Any) {
+        //
+    }
+}
---
+++ Protocol And Delegate Example/Protocol And Delegate Example/Viewcontroller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Protocol And Delegate Example
+//
+//  Created by MacBookPro on 05/02/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Outlets
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblEmail: UILabel!
+    @IBOutlet weak var lblAddress: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK: Function Or Action OnTap Submite Form Button 
+    @IBAction func formButtton(_ sender: Any) {
+        //MARK: Navigation
+    }
+}
+
