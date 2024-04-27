cfe600b538e25069a1d5f45af89d339124964097
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-27 11:39:18 +0530

    initial commit

---
+++ Property-Observer/Property-Observer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Property-Observer
+//
+//  Created by MacBookPro on 07/02/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
