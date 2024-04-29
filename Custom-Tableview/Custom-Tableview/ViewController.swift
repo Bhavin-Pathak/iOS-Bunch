e86a3fb7e8dee94b69283e1633a3142359e1b8d2
Bhavin8289 <bupadhyay277@gmail.com>
2024-04-29 11:29:13 +0530

    chore: tableview examples added

--- Custom-Tableview/Custom-Tableview/ViewController.swift
+++ Custom-Tableview/Custom-Tableview/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    var names = ["Bhavin","Shivam","Sarita","Himanshu","Mayur","Mansi","Divya"]
+    var details = ["iOS Developer","HR","Bank Office","SEO Executive","FullstackDeveloper","Webdeveloper","Nurse"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(names.description)
+        print(details.description)
     }
 
+    //MARK: No of rows in table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Return Details Array
+        return details.count
+       
+    }
+    
+    //MARK: Cell for rows
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") //attech cell identifire with tableview
+        cell?.textLabel?.text = names[indexPath.row] //asign textlable to name array
+        cell?.detailTextLabel?.text = details[indexPath.row] // assign detail to details array
+        return cell!
+            // Simply return cell variable 
+    }
+    
 
 }
 
