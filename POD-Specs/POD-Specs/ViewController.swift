77cc72bcb98ae2b2467e4c64d978be80c6e78996
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-15 09:51:41 +0530

    init

---
+++ POD-Specs/POD-Specs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POD-Specs
+//
+//  Created by MacBookPro on 25/02/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
