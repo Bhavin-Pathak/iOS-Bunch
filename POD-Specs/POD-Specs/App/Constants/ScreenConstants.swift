8ed901b4f77cd1a9b09d9d65ff9f3d6b6e4b200f
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-19 12:56:32 +0530

    -

---
+++ POD-Specs/POD-Specs/App/Constants/ScreenConstants.swift
@@ -0,0 +1,24 @@
+//
+//  ScreenConstants.swift
+//  POD-Specs
+//
+//  Created by MacBookPro on 26/02/1946 Saka.
+//
+
+import Foundation
+import UIKit
+
+
+//MARK: ScreenSize Constant
+struct ScreenConstant{
+    static let leftInset: CGFloat = 10
+    static let topInset: CGFloat = 10
+    static let rightInset: CGFloat = 10
+    static let bottomInset: CGFloat = 10
+    static let borderWidth: CGFloat = 1
+    static let imageheight: CGFloat = 100
+    static let cornerRedious: CGFloat = 10
+    static let wrapperViewInset = UIEdgeInsets(top: 5, left:5, bottom:5, right: 5)
+}
+
+
