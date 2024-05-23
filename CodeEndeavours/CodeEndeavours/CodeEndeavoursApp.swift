d6caf414592ed0df49b76028f2db5bd5032e7229
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-23 12:10:53 +0530

    init

---
+++ CodeEndeavours/CodeEndeavours/CodeEndeavoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeEndeavoursApp.swift
+//  CodeEndeavours
+//
+//  Created by MacBookPro on 02/03/1946 Saka.
+//
+
+import SwiftUI
+
+@main
+struct CodeEndeavoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeEndeavours/CodeEndeavours/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CodeEndeavours
+//
+//  Created by MacBookPro on 02/03/1946 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
