cd206fbf0b76f4a8dfee06d9fbe2b36cf17144a7
Bhavin8289 <bupadhyay277@gmail.com>
2024-05-28 11:09:26 +0530

    here some example of button in swiftui

---
+++ CodeEndeavours/CodeEndeavours/StartingScreen.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeEndeavours
+//
+//  Created by MacBookPro on 02/03/1946 Saka.
+//
+
+import SwiftUI
+
+struct StartingScreen: View {
+    var body: some View {
+      Image(systemName: "power.circle")
+            .resizable()
+            .frame(width: 50,height: 50)
+            .foregroundColor(.indigo)
+            .bold()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartingScreen()
+    }
+}
