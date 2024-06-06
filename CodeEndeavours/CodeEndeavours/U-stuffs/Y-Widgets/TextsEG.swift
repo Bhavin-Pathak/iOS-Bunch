33ae2aceb1abf7e8cf884f7c40604088c024a1fc
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-06 21:55:25 +0530

    -

---
+++ CodeEndeavours/CodeEndeavours/U-stuffs/Y-Widgets/TextsEG.swift
@@ -0,0 +1,21 @@
+//
+//  TextsEG.swift
+//  CodeEndeavours
+//
+//  Created by MacBookPro on 16/03/1946 Saka.
+//
+
+import SwiftUI
+
+struct TextsEG: View {
+    
+    var body: some View {
+       Text("Hello World!!")
+    }
+}
+
+struct TextsEG_Previews: PreviewProvider {
+    static var previews: some View {
+        TextsEG()
+    }
+}
