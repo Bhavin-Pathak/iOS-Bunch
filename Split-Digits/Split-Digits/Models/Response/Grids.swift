b52cad3603c3ba601a83c140e6eec619e4412884
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-11 21:21:39 +0530

    ref-

---
+++ Split-Digits/Split-Digits/Models/Response/Grids.swift
@@ -0,0 +1,15 @@
+//
+//  Grids.swift
+//  Split-Digits
+//
+//  Created by MacBookPro on 21/03/1946 Saka.
+//
+
+import Foundation
+
+struct Box: Identifiable {
+    let id = UUID()
+    let value: Int
+    var isRevealed: Bool = false
+    var isMatched: Bool = false
+}
